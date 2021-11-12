d78a85db795ee5e00652b3c2c3f0cbcaf95f4937
Joy Ho <joyho@macbook-pro.myfiosgateway.com>
2021-11-11 20:47:11 -0500

    data

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-var base = "rgb(255, 255, 255)"
-var accent = "rgb(255, 102, 94)"
+var base = "rgb(255, 255, 255)";
+var accent = "rgb(255, 102, 94)";
+
+var tags = [];
 
 $(document).ready(function(){
 
     $(".tag").click(function(){
         if ($(this).hasClass("active")) {
             $(this).removeClass("active");
+            tags.pop($(this).text());
         } else {
             $(this).addClass("active");
+            tags.push($(this).text());
         }
+        console.log(tags);
     });
 
 });
