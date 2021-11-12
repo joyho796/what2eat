80ee4e28bce310d45509cd3fbd8e67da5e8a9c85
Joy Ho <joyho@macbook-pro.myfiosgateway.com>
2021-11-11 23:53:37 -0500

    img

--- script.js
+++ script.js
@@ -2,18 +2,38 @@ var base = "rgb(255, 255, 255)";
 var accent = "rgb(255, 102, 94)";
 
 var mealList = ["snack", "breakfast", "lunch", "dinner", "side dish"];
-var cuisineList = ["asian", "中", "日", "韩", "中西结合", "western", "italian"];
+var cuisineList = ["asian", "chinese", "japanese", "korean", "中西结合", "western", "italian"];
 var typeList = ["soupy", "炒", "pasta", "meat-rice combo", "noodles", "sandwich", "vegetable"];
 var cookingList = ["microwave", "外卖", "pressure cooker", "pan only", "pan + pot", "锅", "oven"];
 var timeList = ["5 minutes", "15 minutes", "30 minutes", "1 hour", "2 hours"];
 
-function updateList(dataStr, selectedList) {
-    console.log(selectedList);
+function updateList(foods, selectedList) {
+    foods.forEach(function(food){
+        var tags = food.tags;
+        selected = selectedList.flat();
+        if (tags.some(r=> selected.includes(r))) {
+            dataHTML = "";
+            var cuisine = [];
+            tags.forEach(function(tag){
+                if (cuisineList.includes(tag)) {
+                    cuisine.push(tag);
+                }
+            })
+            dataHTML += "<div class='food-item'>";
+            dataHTML += "<img src='" + food.img + "'><div class='food-desc'>";
+            dataHTML += "<span class='food-name'>" + food.name + "</span><br>";
+            dataHTML += "<span class='food-tags'>" + cuisine.join(", ") + "</span><hr>";
+            dataHTML += "<span class='food-ingredients'>" + food.ingredients.join(", ") + "</span><br>";
+            dataHTML += "<span class='food-time'>" + food.time + "</span></div></div>";
+        }
+
+    });
+    $("#food-list").html(dataHTML);    
 }
 
 function shuffle(array) {
   let currentIndex = array.length, randomIndex;
-  
+
   while (currentIndex != 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
